399c363414d0d68ea80c1444c26f1fa00cee33d9
Fabian Bastin <fabian.bastin@gmail.com>
2026-05-30 16:54:17 -0400

    Mod: Modernize Social Media Sharing module (v2.0)

--- mods/social_share/settings.php
+++ mods/social_share/settings.php
@@ -1,51 +1,71 @@
-  <?php
+<?php
 
-  if (!defined("PHORUM_ADMIN")) return;
+if (!defined("PHORUM_ADMIN")) return;
 
-  // If data is posted, then store the posted settings in the database.
-  if (count($_POST))
-  {
-        $PHORUM['social_share']['share_twitter'] = empty($_POST['share_twitter']) ? 0 : 1;
-        $PHORUM['social_share']['share_facebook'] = empty($_POST['share_facebook']) ? 0 : 1;
-        $PHORUM['social_share']['share_google-plus'] = empty($_POST['share_google-plus']) ? 0 : 1;
-        $PHORUM['social_share']['link_new_window'] = empty($_POST['link_new_window']) ? 0 : 1;
+// Default settings
+if (!isset($PHORUM['social_share'])) {
+    $PHORUM['social_share'] = array(
+        'share_twitter'  => 1,
+        'share_facebook' => 1,
+        'share_whatsapp' => 1,
+        'share_linkedin' => 1,
+        'share_telegram' => 1,
+        'share_pinterest'=> 0,
+        'link_new_window'=> 1
+    );
+}
 
-      // The data was okay. Store the settings.
-          phorum_db_update_settings(array("social_share" => $PHORUM["social_share"]));
-          phorum_admin_okmsg('The settings were saved successfully');
-  }
+// If data is posted, then store the posted settings in the database.
+if (count($_POST))
+{
+    $PHORUM['social_share']['share_twitter']  = empty($_POST['share_twitter']) ? 0 : 1;
+    $PHORUM['social_share']['share_facebook'] = empty($_POST['share_facebook']) ? 0 : 1;
+    $PHORUM['social_share']['share_whatsapp'] = empty($_POST['share_whatsapp']) ? 0 : 1;
+    $PHORUM['social_share']['share_linkedin'] = empty($_POST['share_linkedin']) ? 0 : 1;
+    $PHORUM['social_share']['share_telegram'] = empty($_POST['share_telegram']) ? 0 : 1;
+    $PHORUM['social_share']['share_pinterest']= empty($_POST['share_pinterest']) ? 0 : 1;
+    $PHORUM['social_share']['link_new_window']= empty($_POST['link_new_window']) ? 0 : 1;
 
-  // This block is standard for every settings page. The "mod" field
-  // must be set to the name of the module for which the settings
-  // page is written.
-  include_once "./include/admin/PhorumInputForm.php";
-  $frm = new PhorumInputForm ("", "post", "Submit this form");
-  $frm->hidden("module", "modsettings");
-  $frm->hidden("mod", "social_share");
+    // Store the settings.
+    phorum_db_update_settings(array("social_share" => $PHORUM["social_share"]));
+    phorum_admin_okmsg('The settings were saved successfully');
+}
 
-  // Add a header row to the form.
-  $frm->addbreak("Social Media Sharing module settings");
+include_once "./include/admin/PhorumInputForm.php";
+$frm = new PhorumInputForm ("", "post", "Submit this form");
+$frm->hidden("module", "modsettings");
+$frm->hidden("mod", "social_share");
 
-  // Add a text field to the form.
-  $frm->addrow(
-      "Share to Twitter?",
-      $frm->checkbox("share_twitter", "1", "Yes", $PHORUM['social_share']['share_twitter'])
-  );
-  $frm->addrow(
-      "Share to Facebook?",
-      $frm->checkbox("share_facebook", "1", "Yes", $PHORUM['social_share']['share_facebook'])
-  );
-  $frm->addrow(
-      "Share to Google+?",
-      $frm->checkbox("share_google-plus", "1", "Yes", $PHORUM['social_share']['share_google-plus'])
-  );
-  $frm->addrow(
-      "Open Links in New Window?",
-      $frm->checkbox("link_new_window", "1", "Yes", $PHORUM['social_share']['link_new_window'])
-  );
+$frm->addbreak("Social Media Sharing module settings");
 
+$frm->addrow(
+    "Share to Twitter / X?",
+    $frm->checkbox("share_twitter", "1", "Yes", $PHORUM['social_share']['share_twitter'])
+);
+$frm->addrow(
+    "Share to Facebook?",
+    $frm->checkbox("share_facebook", "1", "Yes", $PHORUM['social_share']['share_facebook'])
+);
+$frm->addrow(
+    "Share to WhatsApp?",
+    $frm->checkbox("share_whatsapp", "1", "Yes", $PHORUM['social_share']['share_whatsapp'])
+);
+$frm->addrow(
+    "Share to LinkedIn?",
+    $frm->checkbox("share_linkedin", "1", "Yes", $PHORUM['social_share']['share_linkedin'])
+);
+$frm->addrow(
+    "Share to Telegram?",
+    $frm->checkbox("share_telegram", "1", "Yes", $PHORUM['social_share']['share_telegram'])
+);
+$frm->addrow(
+    "Share to Pinterest?",
+    $frm->checkbox("share_pinterest", "1", "Yes", $PHORUM['social_share']['share_pinterest'])
+);
+$frm->addrow(
+    "Open Links in New Window?",
+    $frm->checkbox("link_new_window", "1", "Yes", $PHORUM['social_share']['link_new_window'])
+);
 
-  // Display the form.
-  $frm->show();
-
-  ?>
+$frm->show();
+?>
--- mods/social_share/social_share.php
+++ mods/social_share/social_share.php
@@ -3,7 +3,6 @@
 if(!defined("PHORUM")) return;
 
 function social_share_mod_css_register($data) {
-
     $data['register'][] = array(
         'module' => 'social_share',
         'where'  => 'after',
@@ -13,46 +12,65 @@ function social_share_mod_css_register($data) {
 }
 
 function social_share_mod_thread($message) {
-global $PHORUM;
-        // Format the read URL.
-        $url = phorum_get_url(
-            PHORUM_FOREIGN_READ_URL,
-            $message['forum_id'], $message['thread'], $message['message_id']
+    global $PHORUM;
+
+    // We only share the first message of a thread or if explicitly called for a message
+    // Usually {HOOK "social_share" TOPIC} or {HOOK "social_share" MESSAGE}
+
+    $url = phorum_get_url(
+        PHORUM_FOREIGN_READ_URL,
+        $message['forum_id'], $message['thread'], $message['message_id']
+    );
+
+    // Strip auth data from the URL, if available.
+    if (isset($_POST[PHORUM_SESSION_LONG_TERM])) {
+        $url = preg_replace(
+            '!,?' . PHORUM_SESSION_LONG_TERM.'=' .
+            urlencode($_POST[PHORUM_SESSION_LONG_TERM]).'!',
+            '', $url
         );
+    }
 
-        // Strip auth data from the URL, if availble.
-        if (isset($_POST[PHORUM_SESSION_LONG_TERM])) {
-            $url = preg_replace(
-                '!,?' . PHORUM_SESSION_LONG_TERM.'=' .
-                urlencode($_POST[PHORUM_SESSION_LONG_TERM]).'!',
-                '', $url
-            );
-        }
+    $encoded_url = urlencode($url);
+    $subject = $message['subject'];
+    $subject = preg_replace("/<img[^>]+\>/i", " ", $subject);
+    $encoded_subject = urlencode($subject);
 
-	$url=urlencode($url);
-	$subject = $message['subject'];
-	$subject = preg_replace("/<img[^>]+\>/i", " ", $subject);
+    $target = ($PHORUM['social_share']['link_new_window'] == 1) ? ' target="_blank" rel="noopener"' : '';
 
-	if ( $PHORUM['social_share']['share_twitter'] == 1 )
-		{
-		print "<a class=\"icon icon-share-twitter\" href=\"http://twitter.com/intent/tweet?source=sharethiscom&text=".$subject."&url=".$url."\"";
-		if ( $PHORUM['social_share']['link_new_window'] == 1 )
-			print " target=\"_blank\"";
-		print ">Tweet</a>";
-		}
-        if ( $PHORUM['social_share']['share_facebook'] == 1 )
-                {
-                print "<a class=\"icon icon-share-facebook\" href=\"https://www.facebook.com/sharer.php?u=".$url."\"";
-                if ( $PHORUM['social_share']['link_new_window'] == 1 )
-                       print " target=\"_blank\"";
-                print ">Facebook</a>";
-                }
-        if ( $PHORUM['social_share']['share_google-plus'] == 1 )
-                {
-                print "<a class=\"icon icon-share-google-plus\" href=\"https://plus.google.com/share?url=".$url."\"";
-                if ( $PHORUM['social_share']['link_new_window'] == 1 )
-                        print " target=\"_blank\"";
-                print ">Google+</a>";
-                }
+    print '<div class="social-share-container">';
+    print '<span class="social-share-label">Partager :</span>';
+
+    // Twitter / X
+    if (!empty($PHORUM['social_share']['share_twitter'])) {
+        print '<a class="social-share-btn ss-twitter" href="https://twitter.com/intent/tweet?text='.$encoded_subject.'&url='.$encoded_url.'"'.$target.' title="Twitter / X"></a>';
+    }
+
+    // Facebook
+    if (!empty($PHORUM['social_share']['share_facebook'])) {
+        print '<a class="social-share-btn ss-facebook" href="https://www.facebook.com/sharer.php?u='.$encoded_url.'"'.$target.' title="Facebook"></a>';
+    }
+
+    // WhatsApp
+    if (!empty($PHORUM['social_share']['share_whatsapp'])) {
+        print '<a class="social-share-btn ss-whatsapp" href="https://api.whatsapp.com/send?text='.$encoded_subject.'%20'.$encoded_url.'"'.$target.' title="WhatsApp"></a>';
+    }
+
+    // LinkedIn
+    if (!empty($PHORUM['social_share']['share_linkedin'])) {
+        print '<a class="social-share-btn ss-linkedin" href="https://www.linkedin.com/sharing/share-offsite/?url='.$encoded_url.'"'.$target.' title="LinkedIn"></a>';
+    }
+
+    // Telegram
+    if (!empty($PHORUM['social_share']['share_telegram'])) {
+        print '<a class="social-share-btn ss-telegram" href="https://t.me/share/url?url='.$encoded_url.'&text='.$encoded_subject.'"'.$target.' title="Telegram"></a>';
+    }
+
+    // Pinterest
+    if (!empty($PHORUM['social_share']['share_pinterest'])) {
+        print '<a class="social-share-btn ss-pinterest" href="https://pinterest.com/pin/create/button/?url='.$encoded_url.'&description='.$encoded_subject.'"'.$target.' title="Pinterest"></a>';
+    }
+
+    print '</div>';
 }
 ?>
